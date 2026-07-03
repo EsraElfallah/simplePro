ea77e17fdd0581b67db897619624f0bb6b6bc022
EsraElfallah <iloveread9@gmail.com>
2026-07-03 23:22:49 +0200

    install whitenoise fix WSGI error

--- simpleApp/urls.py
+++ simpleApp/urls.py
@@ -1,27 +1,9 @@
 from django.urls import path
-from . import views
+from django.http import HttpResponse
 
-urlpatterns=[
-    path('',views.dashboard,name='dashboard'),
-    path('projects',views.projects,name='projects'),
-    path('projects/add/',views.add_project,name='add_project'),
-    path('projects/<int:id>/delete',views.delete_project,name="delete_project"),
-    path('project/<int:id>/edit',views.edit_project,name='edit_project'),
-    path('project_details/<int:id>',views.project_details,name='project_details'),
-    path('expenses',views.expenses,name='expenses'),
-    path('expenses/add/',views.add_expense,name='add_expense'),
-    path('expenses/<int:id>/delete',views.delete_expense,name='delete_expense'),
-    path('expenses/<int:id>/edit',views.edit_expense,name='edit_expense'),
-    path('expenses/<int:id>',views.expense_details,name='expense_details'),
-    path('contributions',views.contributions,name='contributions'),
-    path('contributions/add/',views.add_contribution,name='add_contribution'),
-    path('contributions/<int:id>/delete',views.delete_contribution,name='delete_contribution'),
-    path('contributions/<int:id>/edit',views.edit_contribution,name='edit_contribution'),
-    path('contributions/<int:id>',views.contribution_details,name='contribution_details'),
-    path('categories',views.categories,name='categories'),
-    path('categories/add',views.add_category,name='add_category'),
-    path('categories/<int:id>/delete',views.delete_category,name='delete_category'),
-    path('categories/<int:id>/edit',views.edit_category,name='edit_category'),
-    path('contributers',views.contributers,name='contributers'),
-    
+def test(request):
+    return HttpResponse("WORKING ON RENDER")
+
+urlpatterns = [
+    path('', test),
 ]
