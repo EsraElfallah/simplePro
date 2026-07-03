bdee08027067af19aa814f66054332802c5fc2b0
EsraElfallah <iloveread9@gmail.com>
2026-07-03 23:08:21 +0200

    install whitenoise fix WSGI error

--- simpleProject/settings.py
+++ simpleProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY","dev=secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -123,4 +123,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
